f35482a501cd13f2752e7d1847bcc10d78a646d4
arnaud <arnaudc@google.com>
2021-04-30 13:52:20 -0400

    Implements console compartments

--- hls_js/sdk_less/ui.js
+++ hls_js/sdk_less/ui.js
@@ -17,4 +17,25 @@ class UI {
     get caracteristicsElement (){
         return this.#caracteristicsElement;
     }
+
+    print(eventType, message, alertType){
+        const listItem = document.createElement('li');
+        const timeContainer = document.createElement('span');
+        const eventContainer = document.createElement('span');
+        const messageContainer = document.createElement('span');
+        timeContainer.innerText = this.formatTime();
+        listItem.appendChild(timeContainer);
+        eventContainer.innerText = eventType;
+        listItem.appendChild(eventContainer);
+        messageContainer.innerText = message;
+        listItem.appendChild(messageContainer);
+        listItem.className = 'log-line';
+        this.#caracteristicsElement.appendChild(listItem);
+    }
+
+    formatTime(){
+        let logTime = new Date();
+        logTime = logTime.toTimeString();
+        return logTime.substring(0, logTime.indexOf('GMT') - 1);
+    }
 }
